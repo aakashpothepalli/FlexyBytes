373bfea75513864a64181bae76248e5b767df1fa
psk907 <p.surajkumar907@gmail.com>
2020-02-21 14:53:11 +0530

    profile page

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final _pageOptions = [
        Dashboard(), 
        MyRedeemPage(),
-       StartPage(),
+       ProfilePage(),
       
        
        
--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,14 +1,228 @@
 import 'package:flutter/material.dart';
 
-class StartPage extends StatefulWidget{
-
-SPState createState() => SPState();
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class SPState extends State<StartPage>{
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("yffuyfu"),);
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 2.0),
+        child:ListView(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: 'assets/mkbhd.jpg',
+                child: Container(
+                  height: 125.0,
+                  width: 125.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(62.5),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/mkbhd.jpg')
+                          )
+                          ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Text(
+                'Marques Kumar Brownlee',
+                style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 4.0),
+              Text(
+                'New Jersey',
+                style: TextStyle(fontFamily: 'Montserrat', color: Colors.grey),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '8:57pm',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'CHECK-IN TIME',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.grey),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '315',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'Bytes earned',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.grey),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '21',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'BUCKET LIST',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              /*Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(icon: Icon(Icons.table_chart)),
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              buildImages(),
+              buildInfoDetail(),
+              buildImages(),
+              buildInfoDetail(),*/
+            ],
+          )
+        ],
+      ),),
+    );
   }
-  
+/*
+  Widget buildImages() {
+    return Padding(
+      padding: EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
+      child: Container(
+          height: 200.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              image: DecorationImage(
+                  image: AssetImage('assets/logo_company.png'), fit: BoxFit.cover
+                  )
+                  )
+                  ),
+    );
+  }
+
+  Widget buildInfoDetail() {
+    return Padding(
+      padding:
+          EdgeInsets.only(left: 25.0, right: 25.0, top: 10.0, bottom: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Maldives - 12 Days',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    fontSize: 15.0),
+              ),
+              SizedBox(height: 7.0),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Teresa Soto',
+                    style: TextStyle(
+                        color: Colors.grey.shade700,
+                        fontFamily: 'Montserrat',
+                        fontSize: 11.0),
+                  ),
+                  SizedBox(width: 4.0),
+                  Icon(
+                    Icons.timer,
+                    size: 4.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 4.0),
+                  Text(
+                    '3 Videos',
+                    style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontFamily: 'Montserrat',
+                        fontSize: 11.0),
+                  )
+                ],
+              )
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 20.0,
+                  width: 20.0,
+                  child: Image.asset('assets/navarrow.png'),
+                ),
+              ),
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 20.0,
+                  width: 20.0,
+                  child: Image.asset('assets/chatbubble.png'),
+                ),
+              ),
+              SizedBox(width: 7.0),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 22.0,
+                  width: 22.0,
+                  child: Image.asset('assets/fav.png'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }*/
 }
